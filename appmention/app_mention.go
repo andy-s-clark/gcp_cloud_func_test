2eaaeadb5ce6a96c4d92fb891641107c6e0a39a4
Andy Clark <4296315+andy-s-clark@users.noreply.github.com>
2021-03-14 18:14:29 -0700

    Add publishing

--- appmention/app_mention.go
+++ appmention/app_mention.go
@@ -1,16 +1,53 @@
 package appmention
 
+// When triggered by an "app_mention" event, publish the text to pub/sub
+
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"html"
+	"log"
 	"net/http"
+	"os"
 	"strings"
+
+	"cloud.google.com/go/pubsub"
 )
 
-//func init() {
-//}
+var topic *pubsub.Topic
 
+func init() {
+	projectId := os.Getenv("PROJECT_ID")
+	if projectId == "" {{
+		log.Fatalf("PROJECT_ID is required")
+	}}
+
+	topicName := os.Getenv("TOPIC_NAME")
+	if topicName == "" {{
+		log.Fatalf("TOPIC_NAME is required")
+	}}
+
+	ctx := context.Background()
+	var client *pubsub.Client
+	var err error
+	client, err = pubsub.NewClient(ctx, projectId)
+	if err != nil {
+		log.Fatalf("pubsub.NewClient: %v", err)
+	}
+
+	topic = client.Topic(topicName)
+	exists, err := topic.Exists(ctx)
+	if err != nil {
+		log.Fatalf("pubsub.NewClient.Topic: %v", err)
+	}
+	if !exists {
+		log.Fatalf("Topic " + topicName + " not found")
+	}
+}
+
+// Parsing the event in this function as a learning experience.
+// Normally we'd use the github.com/slack-go/slack/slackevents package and verify tokens, etc.
 type SlackAppMentionEvent struct {
 	Type string `json:"type"`
 	User string `json:"user"`
@@ -20,7 +57,7 @@ type SlackAppMentionEvent struct {
 	EventTs string `json:"event_ts"`
 }
 
-// TODO Verify token, etc.
+
 func AppMention(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		http.Error(w, "405 Method not allowed", http.StatusMethodNotAllowed)
@@ -39,5 +76,13 @@ func AppMention(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "400 Bad Request", http.StatusBadRequest)
 		return
 	}
-	fmt.Fprint(w, html.EscapeString(d.Text))
+	m := &pubsub.Message {
+		Data: []byte("Test message"),
+	}
+	id, err := topic.Publish(r.Context(), m).Get(r.Context())
+	if err != nil {
+		http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprint(w, html.EscapeString(id))
 }
--- appmention/go.mod
+++ appmention/go.mod
@@ -1,3 +1,5 @@
 module example.com/appmention
 
+require cloud.google.com/go/pubsub v1.0.1
+
 go 1.13
